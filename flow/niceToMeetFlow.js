8eaac55af93b6c1412a783c19eaef3293a44eb8e
Your Name <tzafrir@goodquality.net>
2024-03-14 17:26:33 +0200

    adding steps

--- flow/niceToMeetFlow.js
+++ flow/niceToMeetFlow.js
@@ -1,12 +1,14 @@
 const NiceToMeetPage= require  ("../pageobjects/niceToMeet.page.js");
 const faker= require  ("../utils/faker");
 // import twilio from "../utils/twilio";
+const {startStep, endStep, addStep} = require('@wdio/allure-reporter');
 
 
 
 class NiceToMeetFlow {
 
   async basicDetails(requestSender, firstName, lastName, phone, email) {
+    startStep('set user details')
     if (requestSender != null)
       await NiceToMeetPage.selectRequestSender(requestSender);
     if (firstName != null) 
@@ -17,7 +19,10 @@ class NiceToMeetFlow {
       await NiceToMeetPage.setPhone(phone);
     if (email != null) 
       await NiceToMeetPage.setEmail(email);
+    endStep();
+    startStep('click on next button')
     await NiceToMeetPage.clickOnNext();
+    endStep();
   }
 
   async basicDetailsRandom(){
